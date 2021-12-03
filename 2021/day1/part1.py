59c7fd1c3442546730ee89fd877f40cfe3bbdc53
pwalger <paulwalger@protonmail.com>
2021-12-03 22:44:26 +0000

    use list comprehension

--- 2021/day1/part1.py
+++ 2021/day1/part1.py
@@ -5,10 +5,7 @@ increases from the previous measurement.
 
 with open("input.txt", "r") as input_file:
     measurements = [int(m.rstrip()) for m in input_file]
-    count = 0
-
-    for idx in range(1, len(measurements)):
-        if measurements[idx] > measurements[idx - 1]:
-            count += 1
-
+    count = sum(
+        [1 if a > b else 0 for a, b in zip(measurements[1:], measurements[:-1])]
+    )
     print(count)
