c51978b61a3d09f4630f559ffeff562a29bcb8c2
pwalger <paulwalger@protonmail.com>
2022-12-04 11:08:02 +0000

    format day 03

--- 2022/day_03/part1.py
+++ 2022/day_03/part1.py
@@ -1,13 +1,26 @@
 import string
 
-priority_by_letter = {letter: priority for priority, letter in enumerate(string.ascii_letters, 1)}
+priority_by_letter = {
+    letter: priority for priority, letter in enumerate(string.ascii_letters, 1)
+}
+
 
 def main():
     with open("input.txt", "r") as input_file:
         rucksacks = [line.strip() for line in input_file]
-        common_item_types = [(set(rucksack[: len(rucksack) // 2]) & set(rucksack[len(rucksack) // 2: ])).pop() for rucksack in rucksacks]
-        scores = [priority_by_letter[common_item_type] for common_item_type in common_item_types]
+        common_item_types = [
+            (
+                set(rucksack[: len(rucksack) // 2])
+                & set(rucksack[len(rucksack) // 2 :])
+            ).pop()
+            for rucksack in rucksacks
+        ]
+        scores = [
+            priority_by_letter[common_item_type]
+            for common_item_type in common_item_types
+        ]
         print(sum(scores))
+
 
 if __name__ == "__main__":
     main()
--- 2022/day_03/part2.py
+++ 2022/day_03/part2.py
@@ -1,16 +1,23 @@
 import string
 
-priority_by_letter = {letter: priority for priority, letter in enumerate(string.ascii_letters, 1)}
+priority_by_letter = {
+    letter: priority for priority, letter in enumerate(string.ascii_letters, 1)
+}
+
 
 def main():
     with open("input.txt", "r") as input_file:
         rucksacks = [line.strip() for line in input_file]
         common_item_types = [
-            (set(rucksacks[i]) & set(rucksacks[i+1]) & set(rucksacks[i+2])).pop()
+            (set(rucksacks[i]) & set(rucksacks[i + 1]) & set(rucksacks[i + 2])).pop()
             for i in range(0, len(rucksacks), 3)
         ]
-        scores = [priority_by_letter[common_item_type] for common_item_type in common_item_types]
+        scores = [
+            priority_by_letter[common_item_type]
+            for common_item_type in common_item_types
+        ]
         print(sum(scores))
+
 
 if __name__ == "__main__":
     main()
