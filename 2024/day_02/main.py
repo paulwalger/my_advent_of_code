2c7e8f865ca076d85eee568fe1f13d91cd545acc
pwalger <pwalger@superfy.com>
2024-12-03 10:22:52 +0000

    Improve day 02 with itertools.combinations

--- 2024/day_02/main.py
+++ 2024/day_02/main.py
@@ -1,6 +1,6 @@
 # https://adventofcode.com/2024/day/2
 
-from itertools import pairwise
+import itertools
 from typing import Literal
 
 # parse input
@@ -16,7 +16,7 @@ with open("input.txt", "r") as input_file:
 
 def is_report_safe(report: list[int]) -> Literal[0, 1]:
     is_asc = report[0] < report[1]
-    for a, b in pairwise(report):
+    for a, b in itertools.pairwise(report):
         if (a < b) != is_asc or not (1 <= abs(a - b) <= 3):
             return 0
     return 1
@@ -30,13 +30,13 @@ print(f"The total number of safe reports is {number_safe_reports}")
 # part 2
 
 
-def is_report_safe_with_dampener(report: list[int]) -> Literal[0, 1]:
-    for i in range(len(report)):
-        if is_report_safe(report[:i] + report[i + 1 :]):
+def is_report_safe_with_dumpener(report: list[int]) -> Literal[0, 1]:
+    for levels in itertools.combinations(report, len(report) - 1):
+        if is_report_safe(levels):
             return 1
     return 0
 
 
-number_safe_reports_with_dumpener = sum([is_report_safe_with_dampener(report) for report in reports])
+number_safe_reports_with_dumpener = sum([is_report_safe_with_dumpener(report) for report in reports])
 
 print(f"The total number of safe reports with the problem dumpener is {number_safe_reports_with_dumpener}")
